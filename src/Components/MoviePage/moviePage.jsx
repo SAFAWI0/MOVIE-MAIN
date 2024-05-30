ea20468a6e9696872b030c5d58e89a59d3c35864
SAFAWI0 <safaa.ahmad.2000.9@gmail.com>
2024-05-31 01:33:32 +0300

    6commit

--- src/Components/MoviePage/moviePage.jsx
+++ src/Components/MoviePage/moviePage.jsx
@@ -28,7 +28,8 @@ export const MoviePage = () => {
   const [isSaved, setIsSaved] = useState(false);
   const { setIsOpenModal, setMoreInf } = useAppStore();
   const navigate = useNavigate();
-
+ 
+  
   const getData = () => {
     var requestOptions = {
       method: "GET",
@@ -57,15 +58,15 @@ export const MoviePage = () => {
     getData();
   }, [id]);
 
-  // const handleonclick = () => {
-  //   messageApi.open({
-  //     content: "الرجاء تسجيل الدخول",
-  //   });
-  // };
+  const handleonclick = () => {
+    messageApi.open({
+      content: "الرجاء تسجيل الدخول",
+    });
+  };
 
-  // const toggleExpansion = () => {
-  //   setIsExpanded(!isExpanded);
-  // };
+  const toggleExpansion = () => {
+    setIsExpanded(!isExpanded);
+  };
 
   const handelIsBack = () => {
     navigate(-2);
@@ -83,162 +84,17 @@ export const MoviePage = () => {
     setIsSaved(!isSaved);
   };
 
-  // const handleIsOpen = () => {};
+  const handleIsOpen = () => {};
 
-  // const handleOpenModal = () => {
-  //   setIsOpenModal(true);
-  // };
+  const handleOpenModal = () => {
+    setIsOpenModal(true);
+  };
+
+ 
 
   return (
     <div className="categoriesPage">
-      {value && (
-        <div className="content-cat">
-          <div className="image-cover">
-            <img
-              src={`https://image.tmdb.org/t/p/w500${value.poster_path}`}
-              alt={value.title}
-            ></img>
-
-            <div className="header">
-              <div className="content">
-                <IoMdArrowBack className="icon-head" onClick={handelIsBack} />
-                {isSaved ? (
-                  <IoBookmark className="icon-head" onClick={handelIsSave} />
-                ) : (
-                  <CiBookmark className="icon-head" onClick={handelNotSave} />
-                )}
-              </div>
-            </div>
-          </div>
-
-          {/* <div className="Cover">
-            <div className="inCover">
-              <div className="image">
-                <img
-                  src={`https://image.tmdb.org/t/p/w500${value.poster_path}`}
-                  alt={value.title}
-                ></img>
-              </div>
-              <div className="information">
-                <div className="title">
-                  <h3>{value.title}</h3>
-                </div>
-
-                <div className="content-line">
-                  <div className="star">
-                    <span>IMDb:</span>
-                    <p>
-                      {value.vote_average !== undefined
-                        ? value.vote_average.toFixed(1)
-                        : 0}
-                    </p>
-                    <FaStar style={{ color: "gold", marginBottom: "4px" }} />
-                  </div>
-                  <div className="date">
-                    <span>
-                      {value.release_date
-                        ? value.release_date.substring(0, 4)
-                        : ""}
-                    </span>
-                  </div>
-                  <div className="family">
-                    <FaUsers className="icon-family" />
-                  </div>
-                  <div className="view">
-                    <IoEyeOutline className="icon-family" />
-                    <p>
-                      {value.vote_count
-                        ? value.vote_count.toLocaleString()
-                        : ""}
-                    </p>
-                  </div>
-                </div>
-
-                <div className="type">
-                  {value.genres &&
-                    value.genres.map((el, i) => (
-                      <span key={i}>
-                        {el.name}
-                        {i !== value.genres.length - 1 && " | "}
-                      </span>
-                    ))}
-                </div>
-
-                <div className="overview">
-                  <span>
-                    {value.overview &&
-                      (isExpanded || value.overview.length < 100
-                        ? value.overview
-                        : value.overview.substring(0, 100) + "... ")}
-                  </span>
-                  {value.overview && value.overview.length > 100 && (
-                    <button onClick={toggleExpansion}>
-                      {isExpanded ? "Show Less" : "Show More"}
-                    </button>
-                  )}
-                </div>
-
-                <div className="content-line-2">
-                  <div className="butt-play">
-                    <button> Play Now</button>
-                    <FaPlay className="icon-play" />
-                  </div>
-
-                  <div className="eva-view">
-                    <div className="like" onClick={handleonclick}>
-                      <GrDislike className="icon-like" />
-                      <p>
-                        {value.vote_average !== undefined
-                          ? value.vote_average.toFixed(0)
-                          : 0}
-                      </p>
-                    </div>
-
-                    <div className="like" onClick={handleonclick}>
-                      <GrLike className="icon-like" />
-                      <p>{value.vote_count}</p>
-                    </div>
-
-                    <div className="like" onClick={handleonclick}>
-                      <FaRegHeart className="icons" />
-                    </div>
-                    <Link to="https://www.youtube.com/">
-                      <div className="like">
-                        <AiOutlineYoutube className="icons" />
-                      </div>
-                    </Link>
-
-                    <div className="like" onClick={handleOpenModal}>
-                      <TbExclamationMark className="icons" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <Characters />
-
-            <div className="comments">
-              <h3>تعليقات</h3>
-              <LatestComment />
-              <Link to={`/categoriesPage/${id}/comment`}>
-                <div className="more-comment" onClick={handleIsOpen}>
-                  <button>عرض الكل</button>
-                </div>
-              </Link>
-            </div>
-
-            <div className="line" />
-
-            <div className="more-movie">
-              <h3>افلام مشابهة</h3>
-              <Card />
-            </div>
-          </div> */}
-        </div>
-      )}
-      <Footer />
-      {contextHolder}
+     <h1>safaa</h1>
     </div>
   );
 };
