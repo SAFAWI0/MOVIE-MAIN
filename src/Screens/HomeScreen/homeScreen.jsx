9bc099a92cde2e28568005c5689b3adb501741ac
SAFAWI0 <safaa.ahmad.2000.9@gmail.com>
2024-06-27 11:20:34 +0300

    10commit

--- src/Screens/HomeScreen/homeScreen.jsx
+++ src/Screens/HomeScreen/homeScreen.jsx
@@ -1,32 +1,17 @@
-import { useState } from "react";
 import Header from "../../Components/Header/header";
 import BannerSlider from "../../Components/Banner/banner";
 import { Sections } from "../../Components/Sections/sections";
 import { Footer } from "../../Components/Footer/footer";
 import { CategoriesName } from "../../Components/CategoriesName/categoriesName";
-import { SplashScreen } from "../../Components/Logo/splashScreen";
-
 
 export const HomeScreen = () => {
-  const [showSplash, setShowSplash] = useState(true);
-
-  const handleSplashFinish = () => {
-    setShowSplash(false);
-  };
-
   return (
     <div>
-      {showSplash ? (
-        <SplashScreen onFinish={handleSplashFinish} />
-      ) : (
-        <>
-          <Header title="الرئيسية" />
-          <BannerSlider />
-          <CategoriesName />
-          <Sections />
-          <Footer />
-        </>
-      )}
+      <Header title="الرئيسية"/>
+      <BannerSlider />
+      <CategoriesName/>
+      <Sections />
+      <Footer />
     </div>
   );
 };
